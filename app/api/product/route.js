77b59a05ba703d931a6c98585d25e2213ebb6678
MHMasuk <mhmasuk.personal@gmail.com>
2023-08-29 03:20:16 +0600

    Comment product api.

--- app/api/product/route.js
+++ app/api/product/route.js
@@ -1,10 +1,10 @@
-import { NextResponse } from 'next/server'
-
-export async function GET() {
-    const res = await fetch('http://127.0.0.1:8000/api/v1/products/', {
-        next: { revalidate: 3 }, // Revalidate every 60 seconds
-    })
-    const data = await res.json()
-
-    return NextResponse.json(data)
-}
+// import { NextResponse } from 'next/server'
+//
+// export async function GET() {
+//     const res = await fetch('http://127.0.0.1:8000/api/v1/products/', {
+//         next: { revalidate: 3 }, // Revalidate every 60 seconds
+//     })
+//     const data = await res.json()
+//
+//     return NextResponse.json(data)
+// }
