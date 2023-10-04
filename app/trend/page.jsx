722b03344eb16f944f7a93817b3a2113692d8c6f
MHMasuk <mhmasuk.personal@gmail.com>
2023-10-04 12:34:23 +0600

    Fix teesta tank.

--- app/trend/page.jsx
+++ app/trend/page.jsx
@@ -5,12 +5,12 @@ const Trend = () => {
         <div className="h-screen flex justify-center items-center p-4">
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
                 <div className="flex flex-col justify-center items-center">
-                    <img src="https://flood-ffwc.rimes.int/others/teesta-tank-plts/25082023.png" alt="Image 1" className="max-h-screen w-auto" />
+                    <img src="https://flood-ffwc.rimes.int/others/teesta-tank-plts/02102023.png" alt="Image 1" className="max-h-screen w-auto" />
                     {/*<img src="https://flood-ffwc.rimes.int/others/teesta-tank-plts/exceendence20230825.png" alt="Image 1" className="max-h-screen w-auto" />*/}
                 </div>
-                <div className="flex flex-col justify-center items-center">
-                    <img src="https://flood-ffwc.rimes.int/others/teesta-tank-plts/exceendence20230825.png" alt="Image 2" className="max-h-screen w-auto" />
-                </div>
+                {/*<div className="flex flex-col justify-center items-center">*/}
+                {/*    <img src="https://flood-ffwc.rimes.int/others/teesta-tank-plts/exceendence20230825.png" alt="Image 2" className="max-h-screen w-auto" />*/}
+                {/*</div>*/}
             </div>
         </div>
     )
