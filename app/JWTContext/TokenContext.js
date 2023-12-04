180db675ca8be81489978dc20c5cc86a18239525
MHMasuk <mhmasuk.personal@gmail.com>
2023-12-04 19:16:00 +0600

    Fix the issue.

--- app/JWTContext/TokenContext.js
+++ app/JWTContext/TokenContext.js
@@ -1,3 +1,3 @@
-import {useToken} from "@/app/JWTContext/JWTContext";
-
-export const NewDataToken = useToken()
+// import {useToken} from "@/app/JWTContext/JWTContext";
+//
+// export const NewDataToken = useToken()
