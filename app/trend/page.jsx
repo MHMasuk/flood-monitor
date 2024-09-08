38dd8f0d3a45a0f548b42eef7cb03b62b591cd5e
MHMasuk <mhmasuk.personal@gmail.com>
2024-09-08 12:26:08 +0600

    Data.

--- app/trend/page.jsx
+++ app/trend/page.jsx
@@ -57,11 +57,13 @@ const Trend = () => {
         <div className="h-screen flex justify-center items-center p-4">
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
                 <div className="flex flex-col justify-center items-center">
-                    <img src={`https://flood-ffwc.rimes.int/others/teesta-tank-plts/${dischargeDaliaData}`} alt="Image 1" className="max-h-screen w-auto" />
+                    {/*<img src={`https://flood-ffwc.rimes.int/others/teesta-tank-plts/${dischargeDaliaData}`} alt="Image 1" className="max-h-screen w-auto" />*/}
+                    <img src={`https://flood-ffwc.rimes.int/others/teesta-tank-plts/07092024_corr_DL.png`} alt="Image 1" className="max-h-screen w-auto" />
                     {/*<img src={`https://flood-ffwc.rimes.int/others/teesta-tank-plts/${outlookProbabilityDaliaData}`} alt="Image 2" className="max-h-screen w-auto" />*/}
                 </div>
                 <div className="flex flex-col justify-center items-center">
-                    <img src={`https://flood-ffwc.rimes.int/others/teesta-tank-plts/${outlookProbabilityDaliaData}`} alt="Image 2" className="max-h-screen w-auto" />
+                    {/*<img src={`https://flood-ffwc.rimes.int/others/teesta-tank-plts/${outlookProbabilityDaliaData}`} alt="Image 2" className="max-h-screen w-auto" />*/}
+                    <img src={`https://flood-ffwc.rimes.int/others/teesta-tank-plts/exceendence20240907.png`} alt="Image 2" className="max-h-screen w-auto" />
                 </div>
             </div>
         </div>
