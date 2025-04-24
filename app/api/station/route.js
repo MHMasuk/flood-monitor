6c5373cb247e8e15767995becac1f5c4f8cbab84
MHMasuk <mhmasuk.personal@gmail.com>
2025-04-24 17:00:51 +0600

    Add station data.

---
+++ app/api/station/route.js
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+import {getStationData} from "@/utils/getStationData";
+
+// Add this export to prevent prerendering during build time
+export const dynamic = 'force-dynamic';
+
+export async function GET(request, { params }) {
+    // Extract station code from the URL parameter
+    const { stationCode } = params;
+
+    // Call the function to get station data with the provided station code
+    const data = await getStationData(stationCode);
+
+    // Send the data as a JSON response
+    return NextResponse.json(data);
+}
