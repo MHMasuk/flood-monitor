17c563c19149b5b1ef8b10200c7b7032ae10eae3
MHMasuk <mhmasuk.personal@gmail.com>
2026-03-05 12:36:18 +0600

    Fix new chart header and footer.

--- app/comilla/layout.js
+++ app/comilla/layout.js
@@ -2,18 +2,20 @@
 
 import ComillaHeader from "./components/ComillaHeader";
 import ComillaFooter from "./components/ComillaFooter";
-import { LanguageProvider } from "@/app/context/LanguageContext";
+import {LanguageProvider} from "@/app/context/LanguageContext";
 
-export default function ComillaLayout({ children }) {
-  return (
-    <LanguageProvider defaultLanguage="bn">
-      <div className="h-screen flex flex-col overflow-hidden">
-        <ComillaHeader />
-        <main className="flex-1 overflow-hidden pt-16 pb-14">
-          {children}
-        </main>
-        <ComillaFooter />
-      </div>
-    </LanguageProvider>
-  );
+export default function ComillaLayout({children}) {
+    return (
+        <LanguageProvider defaultLanguage="bn">
+            <div className="h-screen flex flex-col overflow-hidden">
+                {/*<ComillaHeader/>*/}
+                <main className="flex-1 overflow-hidden pt-16 pb-14">
+                    <ComillaHeader/>
+                        {children}
+                    <ComillaFooter/>
+                </main>
+                {/*<ComillaFooter/>*/}
+            </div>
+        </LanguageProvider>
+    );
 }
